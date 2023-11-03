4a202a89e965eecbd762d7720b11aeaf106a445d
cassaga <cassagnea@gmail.com>
2023-11-03 15:32:10 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./modules/auth/components/login/login.component";
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./modules/home/home.component";
+import {RegisterComponent} from "./modules/auth/components/register/register.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path :"", component: LoginComponent},
+  {path :"home", component: HomeComponent},
+  {path :"register", component: RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {PostListComponent} from "./modules/post/components/post-list/post-list.component";
+import {CreatePostComponent} from "./modules/post/components/create-post/create-post.component";
+import {LoginComponent} from "./modules/auth/components/login/login.component";
+import {PostDetailComponent} from "./modules/post/components/post-detail/post-detail.component";
+import { HomeComponent } from './modules/home/home.component';
+import { WidgetComponent } from './modules/home/components/widget/widget.component';
+import { RegisterComponent } from './modules/auth/components/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    CreatePostComponent,
+    PostDetailComponent,
+    FooterComponent,
+    LoginComponent,
+    HomeComponent,
+    WidgetComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HeaderComponent,RegisterComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
