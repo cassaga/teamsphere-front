6ebb2b1b50b08cd9af469c3829e7b37d44510887
Antoine <cassagnea@gmail.com>
2023-11-03 16:35:05 +0100

    Commit

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -12,22 +12,6 @@ import {Component} from '@angular/core';
 })
 export class LoginComponent {
 
-log = false;
-reg = false;
-
-    login() {
-      this.log = !this.log;
-      this.reg = false;
-    }
-
-    register() {
-      this.reg = !this.reg;
-      this.log = false;
-    }
-
-    constructor() {
-    }
-
 
 
 }
